299f10845260529b2d7bca14f3e2b105d0670853
Jairo Rubio <rh.jairo.94@gmail.com>
2022-03-13 01:57:49 -0600

    implemented dynamic list of documents per user

--- components/DocumentRow.js
+++ components/DocumentRow.js
@@ -2,9 +2,9 @@ import Button from "@material-tailwind/react/Button";
 import Icon from "@material-tailwind/react/Icon";
 import { userRouter } from "next/dist/client/router"
 
-function DocumentRow(id,fileName,date) {
+function DocumentRow({id,fileName,date}) {
   return ( 
-    <div className="flex items-center p-4 rounded-lg">
+    <div className="flex items-center p-4 rounded-lg text-gray-400">
       <Icon name="article" size="3xl" color="blue" />
       <p className="flex-grow pl-5 w-10 pr-10 truncate">{fileName}</p>
       <p className="pr-5 text-sm">{date?.toDate().toLocaleDateString()}</p>
--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
         >
           Cancel
         </Button>
-        <Button color="blue" onClick={createDocument} ripple="light">
+        <Button color="blue" onClick={createDocument} ripple="dark">
           Create
         </Button>
       </ModalFooter>
@@ -106,7 +106,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="bg-white px-10 md:px-0">
+      <section className="bg-gray-900 px-10 md:px-0">
         <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
           <div className="flex items-center justify-between pb-5">
             <h2 className="font-medium flex-grow">My Documents</h2>
